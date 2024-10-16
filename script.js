88c3b4a4bec273f1f0ca1d42d252d50270b2591a
Pedro Paes Leme <paesp8962@gmail.com>
2024-10-16 15:46:31 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,35 @@
+//Condicionais são uma forma de tomar decisões, de fazer o computadoerexecutar essas decisões.
+
+//if significa "se"
+//else significa "senão"
+
+//if (condição) {código que será executado}
+
+
+//variaveis
+//3 var, Let e const
+//prompt pergunta algo ao usuario;
+//Sempre que usamos dados textuais ""
+//string
+//let nome = prompt("Qual é o seu nome?");
+
+//alert usmos pra exibir uma mensagem
+//alert("Seja bem vindo(a)," + nome)
+
+//let idade = prompt("Quantos anos voce tem?")
+//if significa "se"
+//else significa "senão"
+//if (condição) {código que será executado}
+//if(idade >= 18) {
+    //alert("Voce já pode tirar a carteira de motorista!!!")
+//}else{
+    //alert("Voce ainda não tem idade para dirigir!")
+//}
+
+let GostarDePizza = prompt("Voce gosta de pizza? responda (sim ou não)");
+// === estritamente igual
+if (GostarDePizza === "sim") {
+    alert("Que otimo! Vamos comer juntos!")
+}else {
+    alert("Tudo bem, talvez voce goste  cde outra comida");
+}
